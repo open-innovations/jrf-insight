32ebcfa8c920e6f873b956e0852bd55e0f12bd41
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-05-11 16:49:17 +0100

    Update probe.py and re-run

--- pipelines/extract/probe.py
+++ pipelines/extract/probe.py
@@ -2,66 +2,24 @@ import os
 import pandas as pd
 from statxplore import http_session
 from statxplore import objects
+from utils import get_ids, get_variables, make_csv
 
 STATXPLORE_API_KEY = os.getenv("STATXPLORE_API_KEY")
 session = http_session.StatSession(api_key=STATXPLORE_API_KEY)
 
-# Get a list of all folders on StatXplore
-folders = objects.Schema.list(session)
-children = folders.get('children')
-folder_ids = []
-folder_names = []
-for child in children:
-    folder_ids.append(str(child.get('id')))
-    folder_names.append(str(child.get('label')))
+folder_ids, folder_names = get_ids(session)
 
-for f_id, f_name in zip(folder_ids, folder_names):
-    databases = objects.Schema(f_id).get(session)
-    children = databases.get('children')
-    database_ids = []
-    database_names = []
-    for child in children:
-        database_names.append(child.get('label'))
-        database_ids.append(child.get('id'))
-    #print(f_name, database_names)
-    #print(database_names)
-    for db_id, db_name in zip(database_ids, database_names):
-        schema = objects.Schema(db_id).get(session)
-        #print(schema)
-        children = schema.get('children')
-        measures = []
-        measure_names = []
-        dimensions = []
-        dimension_names = []
+for folder_id, folder_name in zip(folder_ids, folder_names):
+    database_ids, database_names = get_ids(session, locator=folder_id)
 
-        for i in children:
+    for database_id, database_name in zip(database_ids, database_names):
+        measures, measure_names, dimensions, \
+            dimension_names = get_variables(session, database_id)
+        
+        print('Got lookups for database {} in folder {}'.format(database_name, folder_name))
 
-            id = str(i.get('id'))
-            label = str(i.get('label'))
-
-            if "str:measure:" in id or "str:count:" in id:
-                measures.append(id)
-                measure_names.append(label)
-                continue
-            #print('here before count')
-            # if "str:count:" in id:
-            #     measures.append(id)
-            #     measure_names.append(label)
-            #     continue
-            #print('here before dimension')
-            dimensions.append(id)
-            dimension_names.append(label)
-
-        measures_df = pd.DataFrame([[i, j] for i, j in zip(measures, measure_names)], columns=['measure', 'measure_name']).set_index('measure_name')
-        dimensions_df = pd.DataFrame([[l, m] for l, m in zip(dimensions, dimension_names)], columns=['dimension', 'dimension_name']).set_index('dimension_name')
-
-        OUTDIR = 'data/lookups/{folder}/{database}/'.format(folder=f_name, database=db_name)
+        OUTDIR = 'data/lookups/{folder}/{database}/'.format(folder=folder_name, database=database_name)
         os.makedirs(OUTDIR, exist_ok=True)
 
-        dimensions_df.to_csv(os.path.join(OUTDIR, 'dimensions.csv'))
-        measures_df.to_csv(os.path.join(OUTDIR, 'measures.csv'))
-print('done')
-# test_table = objects.Table('str:database:HBAI').run_query(session,
-#     measures=[measures[0]],
-#     dimensions=[[dimensions[0]]],
-# )
+        make_csv(measures, measure_names, OUTDIR, type='measure')
+        make_csv(dimensions, dimension_names, OUTDIR, type='dimension')
