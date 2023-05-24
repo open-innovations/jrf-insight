1ddcd89d223754389b0cd6939bd16153029c3d24
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-05-24 14:11:31 +0100

    Adding some hex maps
    Colours not yet working

--- pipelines/prepare/example.py
+++ pipelines/prepare/example.py
@@ -26,13 +26,19 @@ line_chart = line_chart[(line_chart["Financial Year"] != "2020-21 (covid2021)")
 line_chart = line_chart.pivot(
     index="Financial Year", columns='Location in the United Kingdom of the Household of the Individual (please calculate three-year averages - click on i for the correct method) (3ya)', values='Count')
 
-#bar chart
-bar_chart = data[(data["Type of Individual by Age Category"] != 'Total') & 
+# bar chart
+bar_chart = data[(data["Type of Individual by Age Category"] != 'Total') &
                  (data["Financial Year"] == date_str) &
                  (data["60 per cent of median net household income (BHC) in latest prices"] == "In low income (below threshold)")]
 
 bar_chart = bar_chart.pivot(index="Location in the United Kingdom of the Household of the Individual (please calculate three-year averages - click on i for the correct method) (3ya)",
                             columns="Type of Individual by Age Category", values='Count')
+
+# hex map
+name_to_code = {'Yorkshire and The Humber (E12000003)': 'E12000003', 'North West (E12000002)': 'E12000002', 'North East (E12000001)': 'E12000001'}
+hex_map = bar_chart.rename(index=name_to_code).drop('Total')
+hex_map.index.names = ['geography_code']
+hex_map = hex_map.astype(float, errors='ignore')
 
 # make the output directory
 OUTDIR = 'src/_data/example/'
@@ -42,4 +48,5 @@ os.makedirs(OUTDIR, exist_ok=True)
 dashboard.to_csv(os.path.join(OUTDIR, 'dashboard.csv'))
 line_chart.to_csv(os.path.join(OUTDIR, 'line_chart.csv'))
 bar_chart.to_csv(os.path.join(OUTDIR, 'bar_chart.csv'))
+hex_map.to_csv(os.path.join(OUTDIR, 'hex_map.csv'))
 date_series.to_json(os.path.join(OUTDIR, 'stats.json'), index="orient")
