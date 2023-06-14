053c9024cc5c82ad3b9ff89cde3c4cb39910215b
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-06-14 16:33:21 +0100

    Add a missing lookup

--- pipelines/extract/utils.py
+++ pipelines/extract/utils.py
@@ -107,9 +107,13 @@ def get_variables(session, database_id):
                 print(f'couldn\'t get groups for {id}')
                 continue        
         elif check==False:
-            #print('here')
             dimensions.append(id)
             dimension_names.append(label)
+        #fixing a bug in statxplore - missing variable.
+    if database_id == 'str:database:WP_Sustainment_Ind':
+        dimensions.append("str:field:WP_Attachments:F_WP_DATE:DATE_NAME")
+        dimension_names.append("month")
+    
     return measures, measure_names, dimensions, dimension_names
 
 
