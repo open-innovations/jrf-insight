7ea6a9bc608dac194c9cbd2be792d5b975e9ed4c
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-06-23 16:45:18 +0100

    Melt CLIF csv

--- pipelines/extract/extract.py
+++ pipelines/extract/extract.py
@@ -7,7 +7,7 @@ GEOLOOKUP = "data/geo/geography_lookup.csv"
 if __name__ == '__main__':
     #get the data
     HBAI = query_to_pandas(STATXPLORE_API_KEY, 'pipelines/extract/json/data/HBAI.json')
-    CLIF = query_to_pandas(STATXPLORE_API_KEY, 'pipelines/extract/json/data/CLIF.json')
+    CLIF = query_to_pandas(STATXPLORE_API_KEY, 'pipelines/extract/json/data/CLIF.json').reset_index()
     #slugify the column names
     HBAI.rename(columns=slugify, inplace=True)
 
@@ -16,8 +16,12 @@ if __name__ == '__main__':
     name_to_code_dict = dict(list(zip(GEOLOOKUP_DF.LAD22NM, GEOLOOKUP_DF.LAD22CD)))
     
     CLIF.rename(columns=name_to_code_dict, inplace=True)
+
+    #@TODO rewrite below so its not hard-coded.
+    CLIF_long = pd.melt(CLIF, id_vars=['Year', 'Age of Child (years and bands)', 'Gender of Child', 'Family Type', 'Work Status'], value_vars=CLIF.columns.to_list()[5:], var_name='geography_code').set_index('Year')
+
     #write to csv
     os.makedirs('data/hbai', exist_ok=True)
     os.makedirs('data/clif', exist_ok=True)
     HBAI.to_csv('data/hbai/hbai.csv')
-    CLIF.to_csv('data/clif/clif.csv')
+    CLIF_long.to_csv('data/clif/clif.csv')
