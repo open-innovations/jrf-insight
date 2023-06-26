c8b2c9c52e5c5682879d572bc125b339a9261813
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-06-26 16:49:51 +0100

    Adding HBAI time-series data

--- pipelines/extract/extract.py
+++ pipelines/extract/extract.py
@@ -1,19 +1,32 @@
 import pandas as pd
 from utils import *
 from describe import query_to_pandas
+import json
 
 GEOLOOKUP = "data/geo/geography_lookup.csv"
-
+HBAI_JSON = 'pipelines/extract/json/data/HBAI.json'
 if __name__ == '__main__':
+    dates = ['1011', '1112', '1213', '1314', '1415', '1516', '1617', '1718', '1819', '1920', '2021', '2122']
+    os.makedirs('data/hbai', exist_ok=True)
+    for date in dates:
+        with open(HBAI_JSON, 'r') as json_file:
+            data  = json.load(json_file)
+        
+        data['recodes']['str:field:HBAI:V_F_HBAI:YEAR']['map'] = [[f'str:value:HBAI:V_F_HBAI:YEAR:C_HBAI_YEAR:{date}']]
+    
+        with open(HBAI_JSON, "w") as jsonFile:
+            data = json.dump(data, jsonFile)
+
+        HBAI = query_to_pandas(STATXPLORE_API_KEY, 'pipelines/extract/json/data/HBAI.json')
+        HBAI.rename(columns=slugify, inplace=True)
+        HBAI.to_csv(f'data/hbai/hbai_{date}.csv')
+    
     #get the data
-    HBAI = query_to_pandas(STATXPLORE_API_KEY, 'pipelines/extract/json/data/HBAI.json')
     CLIF = query_to_pandas(STATXPLORE_API_KEY, 'pipelines/extract/json/data/CLIF.json').reset_index()
     #slugify the column names
-    HBAI.rename(columns=slugify, inplace=True)
 
     GEOLOOKUP_DF = pd.read_csv(GEOLOOKUP)
-    #geo_test = GEOLOOKUP_DF[[GEOLOOKUP_DF['RGN22NM'] == 'North East'] or [GEOLOOKUP_DF['RGN22NM'] == 'North West'] or [GEOLOOKUP_DF['RGN22NM'] == 'Yorkshire and The Humber']]
-    #print(geo_test.LAD22NM)
+    
     name_to_code_dict = dict(list(zip(GEOLOOKUP_DF.LAD22NM, GEOLOOKUP_DF.LAD22CD)))
     CLIF.rename(columns=name_to_code_dict, inplace=True, errors="raise")    
 
@@ -21,7 +34,5 @@ if __name__ == '__main__':
     CLIF_long = pd.melt(CLIF, id_vars=['Year', 'Age of Child (years and bands)', 'Gender of Child', 'Family Type', 'Work Status'], value_vars=CLIF.columns.to_list()[5:], var_name='geography_code').set_index('Year')
 
     #write to csv
-    os.makedirs('data/hbai', exist_ok=True)
     os.makedirs('data/clif', exist_ok=True)
-    HBAI.to_csv('data/hbai/hbai.csv')
     CLIF_long.to_csv('data/clif/clif.csv')
