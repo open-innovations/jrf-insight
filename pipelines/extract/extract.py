1a4649c533dc9fde208006d0e5515783cc5d0edc
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-06-26 14:12:38 +0100

    Add error checking in name to code mapping

--- pipelines/extract/extract.py
+++ pipelines/extract/extract.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
     HBAI.rename(columns=slugify, inplace=True)
 
     GEOLOOKUP_DF = pd.read_csv(GEOLOOKUP)
-
+    #geo_test = GEOLOOKUP_DF[[GEOLOOKUP_DF['RGN22NM'] == 'North East'] or [GEOLOOKUP_DF['RGN22NM'] == 'North West'] or [GEOLOOKUP_DF['RGN22NM'] == 'Yorkshire and The Humber']]
+    #print(geo_test.LAD22NM)
     name_to_code_dict = dict(list(zip(GEOLOOKUP_DF.LAD22NM, GEOLOOKUP_DF.LAD22CD)))
-    
-    CLIF.rename(columns=name_to_code_dict, inplace=True)
+    CLIF.rename(columns=name_to_code_dict, inplace=True, errors="raise")    
 
     #@TODO rewrite below so its not hard-coded.
     CLIF_long = pd.melt(CLIF, id_vars=['Year', 'Age of Child (years and bands)', 'Gender of Child', 'Family Type', 'Work Status'], value_vars=CLIF.columns.to_list()[5:], var_name='geography_code').set_index('Year')
