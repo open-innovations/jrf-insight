b2d47966980d424117767c8248af5aa14d2ac388
Giles Dring <giles@dringtech.com>
2023-06-29 12:55:03 +0100

    Adding geojson maps

--- pipelines/notebooks/place/prepare.py
+++ pipelines/notebooks/place/prepare.py
@@ -1,7 +1,9 @@
 import sys
 import os
-import pandas as pd
 import json
+
+import pandas as pd
+import geopandas as gpd
 
 DATA_DIR = '../../../data'
 SRC_DATA_DIR = '../../../src/_data'
@@ -17,6 +19,9 @@ class PlaceMetadata:
     def get_all_codes_for_code(self, code):
         return [code] + self.data[code].get('children', [])
 
+    def get_direct_children_for_code(self, code):
+        return self.data[code].get('direct_children', [])
+
 
 class PlaceData:
     def __init__(self):
@@ -26,8 +31,21 @@ class PlaceData:
         return self.data[self.data.index.isin(codes)]
 
 
+class Shapes:
+    def __init__(self):
+        self.data = gpd.read_parquet(f'{DATA_DIR}/interim/shapes.parquet')
+
+    def filter_by_codes(self, codes):
+        return self.data[self.data.index.isin(codes)]
+
+
 def save_file(data, path):
     data.to_csv(f'{path}/place_data.csv')
+
+
+def save_geojson(data, path, filename):
+    with open(f'{path}/{filename}', 'w') as f:
+        f.write(data.to_json())
 
 
 if __name__ == '__main__':
@@ -57,10 +75,17 @@ if __name__ == '__main__':
     with open(f"{PLACE_DIR}/_data/direct_parents.json", "w") as f:
         f.write(json.dumps(this_place['direct_parents'].tolist()))
 
-    geography_codes = PlaceMetadata().get_all_codes_for_code(geography_code)
+    metadata = PlaceMetadata()
+    geography_codes = metadata.get_all_codes_for_code(geography_code)
 
     place_data = all_place_data.filter_by_codes(geography_codes)
 
     place_data.drop(columns=['children', 'direct_children', 'name', 'type']).pipe(
         save_file, PLACE_DIR + "_data/"
     )
+
+    codes = metadata.get_direct_children_for_code(geography_code)
+    if len(codes) < 1:
+        codes = [geography_code]
+    geojson = Shapes().filter_by_codes(codes)
+    geojson.pipe(save_geojson, PLACE_DIR + "_data/", 'map.geojson')
