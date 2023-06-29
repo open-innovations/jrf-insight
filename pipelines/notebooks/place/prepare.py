b07cb208f1605b6ea9f5b76f3bf3a1146da62dbf
Giles Dring <giles@dringtech.com>
2023-06-29 10:29:07 +0100

    Refactoring place pipeline to use the parquet file

--- pipelines/notebooks/place/prepare.py
+++ pipelines/notebooks/place/prepare.py
@@ -7,51 +7,22 @@ DATA_DIR = '../../../data'
 OUTPUT_DIR = '../../../src/_data'
 
 
-def get_place_metadata(geography_code):
-    place_metadata_file = f'{OUTPUT_DIR}/places.json'
-    with open(place_metadata_file) as f:
-        place_metadata = json.loads(f.read())[geography_code]
-    return place_metadata
+class PlaceMetadata:
+    def __init__(self):
+        place_metadata_file = f'{OUTPUT_DIR}/places.json'
+        with open(place_metadata_file) as f:
+            self.data = json.loads(f.read())
+
+    def get_all_codes_for_code(self, code):
+        return [code] + self.data[code].get('children', [])
 
 
-def get_geography_codes(geography_code, place_metadata):
-    geography_codes = [geography_code]
-    if 'children' in place_metadata:
-        geography_codes = geography_codes + place_metadata['children']
-    return geography_codes
+class PlaceData:
+    def __init__(self):
+        self.data = pd.read_parquet(f'{DATA_DIR}/interim/place_data.parquet')
 
-
-def get_population_data(code_list):
-    population_data = pd.read_csv(
-        f'{DATA_DIR}/population-estimates/population-estimates.csv')
-    return population_data[population_data.geography_code.isin(
-        code_list)].drop_duplicates()
-
-
-def get_council_tax_data(code_list):
-    council_tax_data = pd.read_csv(
-        f'{DATA_DIR}/council-tax-support/council-tax-support.csv')
-    most_recent_date = max(council_tax_data.date)
-    return council_tax_data[council_tax_data.geography_code.isin(
-        code_list) & (council_tax_data.date == most_recent_date)]
-
-def get_children_low_income_data(code_list):
-    clif_data = pd.read_csv(
-        f'{DATA_DIR}/clif/clif_REL.csv')
-    most_recent_date = max(clif_data.Year)
-    clif_data = clif_data[(clif_data['Age of Child (years and bands)'] == 'Total') &
-                          (clif_data['Gender of Child'] == 'Total') &
-                          (clif_data['Family Type'] == 'Total') &
-                          (clif_data['Work Status'] == 'Total')]
-    return clif_data[clif_data.geography_code.isin(code_list)
-                     & (clif_data.Year == most_recent_date)]
-
-def combine_data(*args):
-    return pd.concat(args)
-
-
-def make_table(data):
-    return data.pivot(index='geography_code', columns='variable_name', values='value')
+    def filter_by_codes(self, codes):
+        return self.data[self.data.index.isin(codes)]
 
 
 def save_file(data, path):
@@ -63,16 +34,8 @@ if __name__ == '__main__':
     PLACE_DIR = f'{OUTPUT_DIR}/place_data/{geography_code}/'
     os.makedirs(PLACE_DIR, exist_ok=True)
 
-    place_metadata = get_place_metadata(geography_code)
+    geography_codes = PlaceMetadata().get_all_codes_for_code(geography_code)
 
-    geography_codes = get_geography_codes(geography_code, place_metadata)
-
-    combine_data(
-        get_population_data(geography_codes),
-        get_council_tax_data(geography_codes),
-        get_children_low_income_data(geography_codes)
-    ).pipe(
-        make_table
-    ).pipe(
+    PlaceData().filter_by_codes(geography_codes).pipe(
         save_file, PLACE_DIR
     )
