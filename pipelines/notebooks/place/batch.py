232a565489a8356dd888edfdc1ed3c572c74934e
Giles Dring <giles@dringtech.com>
2023-07-02 22:41:04 +0100

    Adding progress bar to batch

--- pipelines/notebooks/place/batch.py
+++ pipelines/notebooks/place/batch.py
@@ -1,10 +1,13 @@
 import yaml
 
+from progress.bar import Bar
 from prepare import process, shared_data
 
 if __name__ == "__main__":
     shared_data()
     with open('places.yaml') as f:
         places = yaml.safe_load(f.read())
-    for place in places:
-        process(place)
+    with Bar('Processing', max=len(places)) as bar:
+      for place in places:
+          process(place)
+          bar.next()
