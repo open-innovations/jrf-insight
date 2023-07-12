e6756ae46b6e629d56decd52f3c21458b85cca3e
Giles Dring <giles@dringtech.com>
2023-07-12 10:38:41 +0100

    Multi-processing pipeline

--- pipelines/place/batch.py
+++ pipelines/place/batch.py
@@ -1,13 +1,19 @@
+from multiprocessing import Pool
 import yaml
+
 
 from progress.bar import Bar
 from prepare import process, shared_data
+
+chunksize = 4
 
 if __name__ == "__main__":
     shared_data()
     with open('places.yaml') as f:
         places = yaml.safe_load(f.read())
-    with Bar('Processing', max=len(places)) as bar:
-      for place in places:
-          process(place)
-          bar.next()
+    chunks = [places[i:i+chunksize] for i in range(0, len(places), chunksize)]
+
+    with Pool(processes=chunksize) as pool, Bar('Processing', max=len(places)) as bar:
+        for chunk in chunks:
+            pool.map(process, chunk)
+            bar.next(chunksize)
