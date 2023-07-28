7873abfb32e6f40e2bbbcaadf1eb0d4a0b695b03
Christian Spence <mail@christianspence.co.uk>
2023-07-28 13:09:24 +0100

    Reformat homelessness data

--- R/statutory-homelessness.R
+++ R/statutory-homelessness.R
@@ -4,14 +4,30 @@
 
 homelessness_path <- "data-raw/statutory-homelessness/Detailed_LA_202303.ods"
 
-source("https://raw.githubusercontent.com/ChristianSpence/Fixing-column-headers/main/merge_column_names.R")
+homelessness_cols <- c("geography_code",
+                       "geography_name",
+                       "X3",
+                       "X4",
+                       "Total number of households assessed",
+                       "X6",
+                       "Total households assessed as owed a duty",
+                       "Threatened with homelessness",
+                       "Threatened with homelessness (Section 21)",
+                       "Homeless - relief duty owed",
+                       "X11",
+                       "No duty owed",
+                       "X13",
+                       "Number of households in area (000s)",
+                       "Households assessed as threatened with homelessness per (000s)",
+                       "Households assessed as homeless per (000s)"
+                       )
 
-homelessness_data <- readODS::read_ods(homelessness_path, sheet = "A1")
+homelessness_data <- readODS::read_ods(homelessness_path, sheet = "A1", skip = 7, col_names = FALSE, na = "..") |>
+  setNames(homelessness_cols) |>
+  dplyr::select(-(dplyr::starts_with("X"))) |>
+  dplyr::filter(grepl("[A-Z][0-9]{8}", geography_code)) |>
+  tidyr::pivot_longer(-c(geography_code, geography_name), names_to = "variable_name") |>
+  dplyr::filter(variable_name == "Total households assessed as owed a duty") |>
+  dplyr::mutate(date = "2023 Q1", .before = 1)
 
-h2 <- merge_column_names(homelessness_data, 1:6)
-
-h3 <- h2 |>
-  dplyr::select(1, 2, 5) |>
-  dplyr::filter(grepl("[A-Z][0-9]{8}", geography_code))
-
-readr::write_csv(h3, "data/statutory-homelessness/statutory-homelessness.csv")
+readr::write_csv(homelessness_data, "data/statutory-homelessness/statutory-homelessness.csv")
