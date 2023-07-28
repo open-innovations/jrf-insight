b8ac430887a7df0773d993541b069b067977c916
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-07-28 14:17:20 +0100

    Add support with mortgage interest data

--- pipelines/statxplore/extract.py
+++ pipelines/statxplore/extract.py
@@ -6,6 +6,8 @@ import json
 GEOLOOKUP = "data/geo/geography_lookup.csv"
 HBAI_JSON = 'pipelines/statxplore/json/data/HBAI.json'
 CLIF_JSONS = ['pipelines/statxplore/json/data/CLIF_REL.json', 'pipelines/statxplore/json/data/CLIF_ABS.json']
+SMI_households = 'pipelines/statxplore/json/data/SMI_households.json'
+SMI_amount = 'pipelines/statxplore/json/data/SMI_amount.json'
 #dates = ['1011', '1112', '1213', '1314', '1415', '1516', '1617', '1718', '1819', '1920', '2021', '2122']
 
 def houses_below_avg_income():
@@ -72,7 +74,21 @@ def children_in_low_income_families():
         CLIF_long.to_csv(f'data/clif/clif_{JSON[-8:-5]}.csv')
     return
 
+def support_mortgage_interest(JSON, variable_name):
+    SMI = query_to_pandas(STATXPLORE_API_KEY, JSON).reset_index()
+    GEOLOOKUP_DF = pd.read_csv(GEOLOOKUP)
+    name_to_code_dict = dict(list(zip(GEOLOOKUP_DF.RGN22NM, GEOLOOKUP_DF.RGN22CD)))
+    SMI.rename(columns=name_to_code_dict, inplace=True, errors="raise")
+    SMI['Quarter'] = pd.to_datetime(SMI['Quarter'], format='%b-%y').dt.strftime('%Y-%m')
+    SMI = SMI.melt(id_vars=["Quarter"], value_vars=['E12000001', 'E12000002', 'E12000003'], var_name="geography_code")
+    SMI['variable_name'] = variable_name
+    os.makedirs('data/smi', exist_ok=True)
+    SMI.to_csv(f'data/smi/{variable_name}.csv')
+    return SMI
+
 if __name__ == '__main__':
     
     houses_below_avg_income()
     children_in_low_income_families()
+    support_mortgage_interest(SMI_households, 'smi_loans_in_payment_households')
+    support_mortgage_interest(SMI_amount, 'smi_in_payment_amount')
