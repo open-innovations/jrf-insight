0e122d3b90cf9da1fab76cb1240434f52edb576a
Giles Dring <giles@dringtech.com>
2023-08-03 09:05:16 +0100

    Optimise presentation of line charts in housing
    Large CSVs with many columns were killing the render functions. As an interim
    step, the duckdb query is now only returning the data for a given place,
    which speeds things up considerably.
    
    There is more optimisation to complete on this.

--- data/interim/duck.ts
+++ data/interim/duck.ts
@@ -6,10 +6,9 @@ const connection = db.connect();
 
 // Setup data
 connection.query("CREATE TABLE current_rental_prices AS SELECT * FROM './data/interim/current_rental_prices.csv'");
-connection.query("CREATE TABLE house_prices AS SELECT * FROM './data/interim/house_prices.csv'");
+connection.query("CREATE TABLE house_prices AS SELECT * FROM read_csv_auto('./data/interim/house_prices.csv')");
 
-function fakeCSVLoader(query: string) {
-  const rows = connection.query(query);
+function makeFakeCSV(rows: Record<string, unknown>[]) {
   const names = Object.keys(rows[0]);
   return {
     names,
@@ -17,13 +16,50 @@ function fakeCSVLoader(query: string) {
   }
 }
 
+function fakeCSVLoader(query: string) {
+  const rows = connection.query(query);
+  return makeFakeCSV(rows);
+}
+
 // Access functions
 export function getRentalPrices() {
   return fakeCSVLoader('SELECT * FROM current_rental_prices');
 }
 
+
+export function getCurrentRentalPricesForPlace(place: string) {
+  const currentRentalPricesForPlace = connection.prepare('SELECT property_code, COLUMNS(?::VARCHAR) FROM current_rental_prices');
+  try {
+    const data = currentRentalPricesForPlace.query(place)
+    return makeFakeCSV(data);
+  } catch(e) {
+    console.error(e);
+    return {
+      rows: [],
+      names: [],
+    }
+  }
+}
+
 export function getHousePrices() {
   return fakeCSVLoader('SELECT * FROM house_prices');
+}
+
+export function getHousePricesForPlace(place: string) {
+  const query = `SELECT date, ${place} FROM house_prices`;
+  try {
+    const data = connection.query(query).map(({ date, ...rest }) => ({
+      date: new Date(date).toISOString().split('T').shift(),
+      ...rest,
+    }));
+    return makeFakeCSV(data);
+  } catch(e) {
+    console.error(e);
+    return {
+      rows: [],
+      names: [],
+    }
+  }
 }
 
 function cleanup() {
--- src/_data/query.ts
+++ src/_data/query.ts
@@ -1,1 +1,6 @@
 export { getPlaceData, getDataForPlace, getDataForPlaces } from '../../data/interim/data.ts'
+
+export {
+  getCurrentRentalPricesForPlace as current_rental_prices_for_place,
+  getHousePricesForPlace as house_prices_for_place,
+} from '../../data/interim/duck.ts';
