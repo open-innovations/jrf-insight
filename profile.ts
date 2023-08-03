155b9aa3dda65a995a4e2d8c109bb4685474e4f7
Giles Dring <giles@dringtech.com>
2023-08-03 10:12:29 +0100

    Updating profiling code

--- profile.ts
+++ profile.ts
@@ -1,10 +1,20 @@
+console.log('Beginning startup delay...');
 await new Promise((resolve) => {
   setTimeout(() => {
-    console.log('Starting');
+    console.log('Startup delay completed...');
     resolve('Done');
   }, 10000);
 });
 
 await import('lume/cli.ts');
 
-console.log('Finish!');
+console.log('Finished build...');
+
+console.log('Beginning closedown delay...');
+await new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Closedown delay completed...');
+    resolve('Done');
+  }, 10000);
+});
+
