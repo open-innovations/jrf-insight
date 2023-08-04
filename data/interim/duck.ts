62c6a0ab162bc72476883fe1080801a5c26e0f81
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-08-04 13:42:40 +0100

    Adding 3-year averaging for hbai data

--- data/interim/duck.ts
+++ data/interim/duck.ts
@@ -14,7 +14,7 @@ connection.query(
   "CREATE TABLE house_prices AS SELECT * FROM read_csv_auto('./data/interim/house_prices.csv')",
 );
 connection.query(
-  "CREATE TABLE hbai AS SELECT * FROM read_csv_auto('./data/hbai/Type of Individual by Age Category.csv')",
+  "CREATE TABLE hbai AS SELECT * FROM read_csv_auto('./data/interim/hbai_age_category.csv')",
 );
 /*
  * ACCESS FUNCTIONS
@@ -47,7 +47,7 @@ export const getHousePricesForPlace = (place: string) =>
 
 export const typeOfIndividual = (place: string) =>
   runQuery(
-    () => connection.query(`PIVOT (SELECT "Financial Year", "Type of Individual by Age Category", value FROM hbai WHERE geography_code=='${place}' AND variable_name=='In low income (below threshold)') ON "Type of Individual by Age Category" USING AVG(value); `)
+    () => connection.query(`PIVOT (SELECT "date", "Type of Individual by Age Category", value FROM hbai WHERE geography_code=='${place}') ON "Type of Individual by Age Category" USING AVG(value);`)
   );
 
 /**
