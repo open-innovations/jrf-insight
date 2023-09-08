1a30e832f012d10eecc957af3378e2e10190a32c
Christian Spence <mail@christianspence.co.uk>
2023-09-08 11:57:06 +0100

    Update ASHE to include annual wages

--- R/ashe.R
+++ R/ashe.R
@@ -1,12 +1,15 @@
 #ashe weekly pay
 
-ashe_file <- "data-raw/ashe/ashetable82022provisional/PROV - Home Geography Table 8.1a   Weekly pay - Gross 2022.xls"
+ashe_file_8_1a <- "data-raw/ashe/ashetable82022provisional/PROV - Home Geography Table 8.1a   Weekly pay - Gross 2022.xls"
 
-ashe_sheets <- readxl::excel_sheets(ashe_file)
+ashe_file_8_7a <- "data-raw/ashe/ashetable82022provisional/PROV - Home Geography Table 8.7a   Annual pay - Gross 2022.xls"
+
+ashe_sheets <- readxl::excel_sheets(ashe_file_8_1a)
+# "All"
 
 geography_code_name_only <- read_csv("data/geo/geography_code_name_only.csv")
 
-ashe <- readxl::read_excel(ashe_file, sheet = "All", skip = 4) |>
+ashe_8_1a <- readxl::read_excel(ashe_file_8_1a, sheet = "All", skip = 4) |>
   dplyr::mutate(date = "2022") |>
   dplyr::select(date,
                 geography_code = Code,
@@ -15,5 +18,17 @@ ashe <- readxl::read_excel(ashe_file, sheet = "All", skip = 4) |>
                 mean_weekly_wage = Mean) |>
   tidyr::pivot_longer(c(median_weekly_wage, mean_weekly_wage), names_to = "variable_name") |>
   dplyr::filter(geography_code %in% geography_code_name_only$code)
+
+ashe_8_7a <- readxl::read_excel(ashe_file_8_7a, sheet = "All", skip = 4) |>
+  dplyr::mutate(date = "2022") |>
+  dplyr::select(date,
+                geography_code = Code,
+                geography_name = Description,
+                median_annual_wage = Median,
+                mean_annual_wage = Mean) |>
+  tidyr::pivot_longer(c(median_annual_wage, mean_annual_wage), names_to = "variable_name") |>
+  dplyr::filter(geography_code %in% geography_code_name_only$code)
+
+ashe <- dplyr::bind_rows(ashe_8_1a, ashe_8_7a)
 
 readr::write_csv(ashe, "data/ashe/weekly-earnings.csv")
