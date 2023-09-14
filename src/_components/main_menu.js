73b92656838d597769658557e7b701cdf8e0f68f
Giles Dring <giles@dringtech.com>
2023-09-14 11:17:39 +0100

    Reorder spotlight, place, data

--- src/_components/main_menu.js
+++ src/_components/main_menu.js
@@ -3,10 +3,11 @@ export default function ({ search, comp }) {
   const spotlights = search.pages('spotlight main', 'title').map(({ title, target }) => ({ title, url: target }));
 
   const items = [
-    { title: 'Places', children: places },
     { title: 'Spotlights', children: spotlights },
+    { title: 'Places', children: places },
     { title: 'Data', url: '/metadata/' },
     { title: 'About', url: '/about/' },
+    // TODO make this float to the right and have a magnifying glass icon
     { title: 'Search', url: '/search/' },
   ];
 
