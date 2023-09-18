7b10766ed681f882a7998b18032e68b83fd22d4d
Giles Dring <giles@dringtech.com>
2023-09-18 14:54:18 +0100

    Get only latest claimant data

--- src/_data/dataset/claimant.ts
+++ src/_data/dataset/claimant.ts
@@ -4,10 +4,13 @@ export const get_claimants_proportion_for_place = (placeCode: string) => {
   const data = run(
     () =>
       connection.query(`
-        SELECT CAST(value as DOUBLE) as value
+        SELECT date, CAST(value as DOUBLE) as value
         FROM claimants
         WHERE geography_code=='${placeCode}'
-        AND variable_name=='Claimants as a proportion of residents aged 16-64';
+        AND variable_name=='Claimants as a proportion of residents aged 16-64'
+        ORDER BY date DESC
+        LIMIT 1
+        ;
       `)
   );
   console.log(data);
