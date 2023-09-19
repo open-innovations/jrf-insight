27ea67bcd88b1746b2ddc7ef999a302bff9348c1
Giles Dring <giles@dringtech.com>
2023-09-19 15:50:45 +0100

    Bring R scripts under partial dvc control
    Most stages `frozen`, for now

---
+++ R/deps.R
@@ -0,0 +1,5 @@
+# This file exists to trick renv into recognising dependencies that aren't directly in the files in this directory
+
+# Added this as it's a depdency of a directly loaded R script
+# This is a horrible kludge to make it work! ;-)
+library(lubridate)
