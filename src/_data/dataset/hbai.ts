53e9c82b395e21b96b950f4c4048f48d3d94cfbf
Giles Dring <giles@dringtech.com>
2023-09-19 17:55:44 +0100

    Fix issue with savings

--- src/_data/dataset/hbai.ts
+++ src/_data/dataset/hbai.ts
@@ -5,7 +5,10 @@ export const low_income_type_of_individual = (place: string) => {
   const data = run(
     () => connection.query(`
       PIVOT (
-        SELECT "date", "Type of Individual by Age Category", percent
+        SELECT
+          "date",
+          "Type of Individual by Age Category",
+          percent
         FROM hbai_by_age_category
         WHERE geography_code=='${place}'
         AND income_status=='In low income (below threshold)'
@@ -16,3 +19,23 @@ export const low_income_type_of_individual = (place: string) => {
   );
   return makeFakeCSV(data);
 };
+
+export const get_savings_and_investments_for_place = (place: string) => {
+  const data = run(
+    () => connection.query(`
+      PIVOT (
+        SELECT
+          date,
+          "Savings and Investments of Adults in the Family of the Individual",
+          percent
+        FROM hbai_savings_investments
+        WHERE geography_code=='${place}'
+        AND income_status=='In low income (below threshold)'
+      )
+      ON "Savings and Investments of Adults in the Family of the Individual"
+      USING AVG(percent)
+      ORDER BY date asc;
+    `)
+  );
+  return makeFakeCSV(data);
+}
