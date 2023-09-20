6733a42186acdac1318f834a634797bd66a9dc1a
Giles Dring <giles@dringtech.com>
2023-09-20 12:39:01 +0100

    Tidy up model for EI

--- src/_data/data/model.ts
+++ src/_data/data/model.ts
@@ -5,10 +5,9 @@ export const econonmic_insecurity = (children: string[]) => {
   if (!(children.length > 0)) {
     return [];
   }
-  const data = run(
+  return run(
     () => connection.query(`
     SELECT * FROM esm WHERE geography_code IN ${arrayToDuckSet(children)};
     `)
   );
-  return makeFakeCSV(data);
 };
