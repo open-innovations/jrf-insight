f916d248e6cafb5b2054994e7a78b248122cf7ba
Christian Spence <mail@christianspence.co.uk>
2023-11-10 12:57:56 +0000

    Update house-prices to download and process and write out parquet

--- R/house-prices.R
+++ R/house-prices.R
@@ -1,8 +1,47 @@
 # house-prices
 
+
+
+# download ----------------------------------------------------------------
+
+# wards
+hpssa_ward_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/housing/datasets/medianpricepaidbywardhpssadataset37"
+
+hpssa_ward_links <- rvest::read_html(hpssa_ward_url) |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
+
+# use the first (i.e. most recent) .zip link
+hpssa_ward_zip <- hpssa_ward_links[grepl(".zip$", hpssa_ward_links)][1]
+
+hpssa_ward_path <- file.path("data-raw", "house-prices", basename(hpssa_ward_zip))
+
+download.file(paste0("http://ons.gov.uk", hpssa_ward_zip),
+              hpssa_ward_path, mode = "wb")
+
+unzip(hpssa_ward_path, exdir = "data-raw/house-prices")
+
+
+# admin geographies
+hpssa_admin_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/housing/datasets/medianhousepricefornationalandsubnationalgeographiesquarterlyrollingyearhpssadataset09"
+
+hpssa_admin_links <- rvest::read_html(hpssa_admin_url) |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
+
+# use the .xls link
+hpssa_admin_xls <- hpssa_admin_links[grepl(".xls[x]*$", hpssa_admin_links)]
+
+hpssa_admin_path <- file.path("data-raw", "house-prices", basename(hpssa_admin_xls))
+
+download.file(paste0("http://ons.gov.uk", hpssa_admin_xls),
+              hpssa_admin_path, mode = "wb")
+
+# process -----------------------------------------------------------------
+
 geography_code_name_only <- readr::read_csv("data/geo/geography_code_name_only.csv")
 
-hpssa_ward <- readxl::read_excel("data-raw/house-prices/hpssadataset37medianpricepaidbyward/HPSSA Dataset 37 - Median price paid by ward.xls", sheet = "1a", skip = 5, na = ":") |>
+hpssa_ward <- readxl::read_excel("data-raw/house-prices/HPSSA Dataset 37 - Median price paid by ward.xls", sheet = "1a", skip = 5, na = ":") |>
   dplyr::select(-(1:2)) |>
   dplyr::rename(geography_code = `Ward code`,
                 geography_name = `Ward name`) |>
@@ -10,6 +49,7 @@ hpssa_ward <- readxl::read_excel("data-raw/house-prices/hpssadataset37medianpric
   dplyr::mutate(variable_name = "Median house price") |>
   dplyr::mutate(date = as.Date(sub("Year ending ", "01 ", date), format = "%d %b %Y")) |>
   dplyr::filter(geography_code %in% geography_code_name_only$code)
+
 
 sheets <- c("1a", "2a", "3a")
 
@@ -36,3 +76,4 @@ hpssa <- dplyr::bind_rows(hpssa_ward,
                           hpssa_admin)
 
 readr::write_csv(hpssa, "data/house-prices/house-prices.csv")
+arrow::write_parquet(hpssa, "data-mart/house-prices/house-prices.parquet")
