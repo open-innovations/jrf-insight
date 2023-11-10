8df555583a4eef485c07179d8f92eef5fe849ad0
Christian Spence <mail@christianspence.co.uk>
2023-11-10 14:42:01 +0000

    Update mortgage-landlord-possession to download and write parquet

--- R/mortgage-landlord-possession.R
+++ R/mortgage-landlord-possession.R
@@ -1,6 +1,26 @@
 # https://www.gov.uk/government/collections/mortgage-and-landlord-possession-statistics
 
+collection_url <- "https://www.gov.uk/government/collections/mortgage-and-landlord-possession-statistics"
+
+links <- rvest::read_html(collection_url) |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
+
+links_to_follow <- links[grepl("mortgage-and-landlord-possession-statistics", links)]
+
+# grab the first one
+page <- rvest::read_html(paste0("https://www.gov.uk", links_to_follow[1])) |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
+
+file_to_grab <- page[grepl(".zip", page)] |>
+  unique()
+
 folder <- 'data-raw/mortgage-landlord-possession'
+local_zip_path <- file.path(folder, basename(file_to_grab))
+
+download.file(file_to_grab, local_zip_path, mode = "wb")
+
 zips <- list.files(folder, pattern = '*.zip', full.names = TRUE)
 
 lapply(zips, function(file) {
@@ -10,7 +30,7 @@ lapply(zips, function(file) {
 })
 
 file <- list.files(folder, pattern = ".csv",
-                   full.names = TRUE, recursive = TRUE)
+                   full.names = TRUE)
 
 possession <- readr::read_csv(file) |>
   tidyr::unite('date', year:quarter, sep = " ") |>
@@ -20,3 +40,6 @@ possession <- readr::read_csv(file) |>
                 possession_type,
                 possession_action,
                 value)
+
+readr::write_csv(possession, 'data/mortgage-landlord-possession/mortgage-landlord-possession.csv')
+arrow::write_parquet(possession, 'data-mart/mortgage-landlord-possession/mortgage-landlord-possession.parquet')
