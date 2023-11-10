53b638665a30c35e6652bf76230804003e18d97d
Christian Spence <mail@christianspence.co.uk>
2023-11-10 14:27:28 +0000

    Update council-tax-support to download and write out parquet

--- R/local-council-tax-support.R
+++ R/local-council-tax-support.R
@@ -1,21 +1,36 @@
 # collection at https://www.gov.uk/government/statistical-data-sets/live-tables-on-local-government-finance#local-council-tax-support
 
-local_ct_support_file <- list.files('data-raw/council-tax-support',
-                                    full.names = TRUE)
+collection_url <- "https://www.gov.uk/government/statistical-data-sets/live-tables-on-local-government-finance"
+
+links <- rvest::read_html(collection_url) |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
+
+target_file <- links[grepl("Local_Council_Tax_Support", links)] |>
+  unique()
+
+file_path <- file.path("data-raw", "council-tax-support", basename(target_file))
+download.file(target_file,
+              file_path,
+              mode = "wb")
+
+local_ct_support_file <- file_path
 sheets <- readODS::list_ods_sheets(local_ct_support_file)
 sheets <- sheets[grepl('Table_[23]', sheets)] |>
   setNames(c('pensioners', 'working_age'))
 
 local_ct_support <- lapply(sheets, function(sht) {
-  readODS::read_ods(local_ct_support_file, sheet = sht,
+  x <- readODS::read_ods(local_ct_support_file, sheet = sht,
                     skip = 3, na = c('-', '[x]', '[z]', '....')
-  ) |>
-    dplyr::select(-dplyr::contains('Percentage'),
-                  -Notes) |>
+  )
+
+  names(x) <- stringr::str_replace_all(names(x), "\\.", " ")
+  x <- x |>
+    dplyr::select(-dplyr::contains('Percentage'), -Notes) |>
     tidyr::pivot_longer(-(1:5), names_to = 'date') |>
     dplyr::mutate(date = substr(date, 7, nchar(date) - 5) |>
                     as.Date(format = "%d %B %Y")) |>
-    dplyr::rename(geography_code.ba = `E-code`,
+    dplyr::rename(geography_code.ba = `E code`,
                   geography_code = `ONS Code`,
                   geography_name = `Local Authority`,
                   geography_type = Class)
@@ -26,12 +41,5 @@ local_ct_support <- lapply(sheets, function(sht) {
                 variable_code, variable_name,
                 value)
 
-source(url('https://raw.githubusercontent.com/economic-analytics/edd/main/R/build-nomis-datasets.R'))
-source(url('https://raw.githubusercontent.com/economic-analytics/edd/main/R/utils-date-formats.R'))
-
-out <- local_ct_support |>
-  df_to_edd_df() |>
-  edd_df_to_edd_obj()
-
 readr::write_csv(local_ct_support, 'data/council-tax-support/council-tax-support.csv')
-saveRDS(out, 'data/council-tax-support/council-tax-support.rds')
+arrow::write_parquet(local_ct_support, 'data-mart/council-tax-support/council-tax-support.parquet')
