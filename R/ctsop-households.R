298bd6e9b0c4dabbe86e585233478e2e2604f440
Christian Spence <mail@christianspence.co.uk>
2023-11-10 11:10:57 +0000

    Update households/ctsop to be self-contained and add parquet write out

--- R/ctsop-households.R
+++ R/ctsop-households.R
@@ -1,17 +1,63 @@
 # ctsop
 
-CTSOP4_1_2022_03_31 <- readr::read_csv("~/Data/VOA/CTSOP/CTSOP4-1-1993-2022/CTSOP4_1_2022_03_31.csv")
+# Council Tax Stock of Properties
+# Published as a collection at https://www.gov.uk/government/collections/valuation-office-agency-council-tax-statistics
 
-CTSOP4_1_2022_03_31$geography |> unique()
+# Download file -----------------------------------------------------------
+
+collection_url <- "https://www.gov.uk/government/collections/valuation-office-agency-council-tax-statistics"
+
+a_tags <- rvest::read_html(collection_url) |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
+
+ctsop_a_tags <- a_tags[grepl("council-tax-stock-of-properties", a_tags)]
+
+latest_ctsop_a_tag <- ctsop_a_tags[1]
+
+# pull html from this latest tag
+latest_page <- rvest::read_html(paste0("https://www.gov.uk", latest_ctsop_a_tag)) |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
+
+latest_page_4_1 <- latest_page[grepl("CTSOP4_1.*", latest_page)] |>
+  unique() # unique as both text and image are <a>
+
+# identify which file is the most recent
+dates <- stringr::str_extract_all(latest_page_4_1, "[0-9]{4}") |>
+  lapply(function(x) max(x)) |>
+  unlist()
+latest_index <- which(dates == max(dates))
+
+target_file <- latest_page_4_1[latest_index]
+
+local_zip_path <- file.path("data-raw", "households", basename(target_file))
+
+download.file(target_file, local_zip_path, mode = "wb")
+
+files_in_zip <- unzip(local_zip_path, list = TRUE)
+
+# extract years from filenames
+max_year <- stringr::str_extract(files_in_zip$Name, "[0-9]{4}") |> max()
+file_to_unzip <- files_in_zip$Name[grepl(max_year, files_in_zip$Name)]
+unzip(local_zip_path, files = file_to_unzip, exdir = file.path("data-raw", "households"))
+
+ctsop_file <- list.files(file.path("data-raw", "households"), pattern = ".csv$", full.names = TRUE)
+
+# Process file ------------------------------------------------------------
+
+ctsop_data <- readr::read_csv(ctsop_file)
+
+ctsop_data$geography |> unique()
 # [1] "ENGWAL" "NATL"   "REGL"   "LAUA"   "MSOA"   "LSOA"   "CTYMET" "UNMD"
 
-households_lad <- CTSOP4_1_2022_03_31 |>
+households_lad <- ctsop_data |>
   # filter for LAUA only
   dplyr::filter(geography == 'LAUA') |>
   # filter for all bands
   dplyr::filter(band == 'All') |>
   # add date and variable
-  dplyr::mutate(date = '2022',
+  dplyr::mutate(date = max_year,
                 variable_name = "Number of households",
                 all_properties = as.integer(all_properties)) |>
   # reduce cols
@@ -20,13 +66,13 @@ households_lad <- CTSOP4_1_2022_03_31 |>
                 variable_name,
                 value = all_properties)
 
-households_wd <- CTSOP4_1_2022_03_31 |>
+households_wd <- ctsop_data |>
   # filter for LAUA only
   dplyr::filter(geography == 'LSOA') |>
   # filter for all bands
   dplyr::filter(band == 'All') |>
   # add date and variable
-  dplyr::mutate(date = '2022',
+  dplyr::mutate(date = max_year,
                 variable_name = "Number of households",
                 all_properties = as.integer(all_properties)) |>
   # reduce cols
@@ -35,7 +81,7 @@ households_wd <- CTSOP4_1_2022_03_31 |>
                 variable_name,
                 value = all_properties)
 
-lsoa_wd <- readxl::read_excel("~/Data/Geodata/Lookups/LSOA11_WD21_LAD21_EW_LU_V2.xlsx") |>
+lsoa_wd <- readxl::read_excel("data/geo/LSOA11_WD21_LAD21_EW_LU_V2.xlsx") |>
   dplyr::select(LSOA11CD, WD21CD)
 
 joined <- dplyr::inner_join(households_wd, lsoa_wd,
@@ -103,5 +149,6 @@ households <- dplyr::bind_rows(joined,
                                panrgn)
 
 readr::write_csv(households, 'data/households/households.csv')
+arrow::write_parquet(households, 'data-mart/households/households.parquet')
 
 
