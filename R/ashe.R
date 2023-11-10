34982a88e03d5af7d42017773a5d0bc20e17a290
Christian Spence <mail@christianspence.co.uk>
2023-11-10 09:55:54 +0000

    ashe.R now self-contained with download and unzip. Parquet and CSV files added

--- R/ashe.R
+++ R/ashe.R
@@ -1,11 +1,38 @@
 #ashe weekly pay
 
-ashe_file_8_1a <- "data-raw/ashe/ashetable82022provisional/PROV - Home Geography Table 8.1a   Weekly pay - Gross 2022.xls"
+# data from ashe table 8
+# https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/earningsandworkinghours/datasets/placeofresidencebylocalauthorityashetable8
 
-ashe_file_8_7a <- "data-raw/ashe/ashetable82022provisional/PROV - Home Geography Table 8.7a   Annual pay - Gross 2022.xls"
+collection_url <- "https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/earningsandworkinghours/datasets/placeofresidencebylocalauthorityashetable8"
+
+a_tags <- rvest::read_html(collection_url) |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
+
+a_tags_with_zip <- a_tags[grepl(".zip$", a_tags)]
+
+# take the first one and append to base URL (without HTTPS for some reason!)
+download_target <- paste0("http://ons.gov.uk", a_tags_with_zip[1])
+
+local_zip_file <- file.path("data-raw/ashe", basename(download_target))
+
+download.file(url = download_target,
+              destfile = local_zip_file,
+              mode = "wb"
+)
+
+# identify file paths to unzip (not doing all of them to save space)
+files_to_unzip <- unzip(local_zip_file, list = TRUE)
+files_to_unzip <- files_to_unzip$Name[grepl("8.1a|8.7a", files_to_unzip$Name)]
+
+unzip(local_zip_file, files = files_to_unzip, exdir = "data-raw/ashe")
+
+ashe_file_8_1a <- file.path("data-raw/ashe", files_to_unzip[grepl("8.1a", files_to_unzip)])
+
+ashe_file_8_7a <- file.path("data-raw/ashe", files_to_unzip[grepl("8.7a", files_to_unzip)])
 
 ashe_sheets <- readxl::excel_sheets(ashe_file_8_1a)
-# "All"
+# We'll be using sheet "All"
 
 geography_code_name_only <- readr::read_csv("data/geo/geography_code_name_only.csv")
 
@@ -36,3 +63,4 @@ ashe_8_7a <- readxl::read_excel(ashe_file_8_7a, sheet = "All", skip = 4, na = c(
 ashe <- dplyr::bind_rows(ashe_8_1a, ashe_8_7a)
 
 readr::write_csv(ashe, "data/ashe/weekly-earnings.csv")
+arrow::write_parquet(ashe, "data-mart/ashe/weekly-earnings.parquet")
